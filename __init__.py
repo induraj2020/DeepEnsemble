7f6301736d7e12323b0b325b7dafa403c1814bd4
Indu <i.ramamurthy@joovence.com>
2021-08-02 05:52:18 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from DeepEnsemble import DeepEnsembler
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='deep-ensemble',
+     version='0.1',
+     scripts=['DeepEnsemble'] ,
+     author="Induraj P.Ramamurthy",
+     author_email="induraj.gandhian@yahoo.com",
+     description="An Wrapper for performing ensembling techniques on deep learning models",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/DataPreprocessing/DataCleaning",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Intended Audience :: Science/Research',
+         'License :: OSI Approved :: MIT License',
+         'Natural Language :: English',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python :: 3.7',
+         'Topic :: Software Development :: Libraries :: Python Modules',
+         'Topic :: Scientific/Engineering :: Artificial Intelligence',
+     ],
+    python_requires=">=3.6"
+ )
